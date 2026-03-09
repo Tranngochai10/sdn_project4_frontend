807978f634734801b525960d51d3514ca2c1df60
haitnse182387@fpt.edu.vn <haitnse182387@fpt.edu.vn>
2026-03-09 10:04:44 +0700

    update api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+// const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://sdn-project4-backend.onrender.com';
 
 const api = axios.create({
     baseURL: BASE_URL,
